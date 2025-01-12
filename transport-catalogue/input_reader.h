045a3cb6031854a7166ebf52d8f1d9041d8e5612
aksi321 <96183002+aksi321@users.noreply.github.com>
2025-01-12 22:46:21 +0300

    input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,36 @@
-// место для вашего кода
+#pragma once
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "geo.h"
+#include "transport_catalogue.h"
+
+namespace input_pars{
+struct CommandDescription {
+    explicit operator bool() const {
+        return !command.empty();
+    }
+
+    bool operator!() const {
+        return !operator bool();
+    }
+
+    std::string command;      
+    std::string id;           
+    std::string description;  
+};
+
+using namespace catalogue;
+
+class InputReader {
+public:
+
+    void ParseLine(std::string_view line);
+
+    void ApplyCommands(TransportCatalogue& catalogue) const;
+
+private:
+    std::vector<CommandDescription> commands_;
+};
+}
