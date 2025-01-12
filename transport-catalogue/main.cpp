7d19947623ef78de9681aac83666d47d4f5a8264
aksi321 <96183002+aksi321@users.noreply.github.com>
2025-01-12 22:46:53 +0300

    main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,33 @@
-// место для вашего кода
+#include <iostream>
+#include <string>
+
+#include "input_reader.h"
+#include "stat_reader.h"
+
+using namespace std;
+
+int main() {
+    TransportCatalogue catalogue;
+
+    int base_request_count;
+    cin >> base_request_count >> ws;
+
+    {
+        using namespace input_pars;
+        InputReader reader;
+        for (int i = 0; i < base_request_count; ++i) {
+            string line;
+            getline(cin, line);
+            reader.ParseLine(line);
+        }
+        reader.ApplyCommands(catalogue);
+    }
+    using namespace utility;
+    int stat_request_count;
+    cin >> stat_request_count >> ws;
+    for (int i = 0; i < stat_request_count; ++i) {
+        string line;
+        getline(cin, line);
+        ParseAndPrintStat(catalogue, line, cout);
+    }
+}
