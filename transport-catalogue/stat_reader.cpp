d7d9cbf60e1cacced5bb27a2ac096da291c09aac
aksi321 <96183002+aksi321@users.noreply.github.com>
2025-02-06 20:28:13 +0300

    9 спринт stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,46 +1,45 @@
 #include "stat_reader.h"
 #include <iostream>
 
-namespace utility{
+namespace utility {
 
-void PrintBus(const TransportCatalogue& tansport_catalogue, std::string_view left, std::string_view rigth, std::ostream& output  ){
-    BusCounted rez;
-    rez = tansport_catalogue.GetBusStatistics(rigth);
-    if (!rez.IsEmpty()){
-            output<<left<<" "<<rigth<<": "<<rez.amount<<" stops on route, "<<rez.unique<<" unique stops, "<<rez.length<<" route length";
+void PrintBus(const TransportCatalogue& catalogue, std::string_view left, std::string_view right, std::ostream& output) {
+    BusCounted rez = catalogue.GetBusStatistics(right);
+    if (!rez.IsEmpty()) {
+        double curvature = (rez.geo_length > 0) ? rez.length / rez.geo_length : 0.0;
+        output << left << " " << right << ": " << rez.amount << " stops on route, " 
+               << rez.unique << " unique stops, " << rez.length << " route length, " 
+               << curvature << " curvature";
     } else {
-        output<<left<<" "<<rigth<<": not found";
+        output << left << " " << right << ": not found";
     }
-
 }
 
-void PrintStop(const TransportCatalogue& tansport_catalogue, std::string_view left, std::string_view rigth, std::ostream& output  ){
-    BusesForStop  answer =tansport_catalogue.GetBusesForStop(rigth);
-        output<<left<<" "<<rigth<<":";
-        if (answer.request_status == "not found"){
-            output<<" not found";
-        } else if (answer.request_status == "empty"){
-            output<< " no buses";
-        } else if (answer.request_status == "found"){
-            output<<" buses";
-            for (auto s : answer.buses){
-                output<<" "<<s;
-            }
+void PrintStop(const TransportCatalogue& catalogue, std::string_view left, std::string_view right, std::ostream& output) {
+    BusesForStop answer = catalogue.GetBusesForStop(right);
+    output << left << " " << right << ":";
+    if (answer.request_status == "not found") {
+        output << " not found";
+    } else if (answer.request_status == "empty") {
+        output << " no buses";
+    } else if (answer.request_status == "found") {
+        output << " buses";
+        for (auto s : answer.buses) {
+            output << " " << s;
         }
+    }
 }
 
-void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
-                       std::ostream& output) {   
-    std::string_view answer;                  
-    size_t pos = request.find(' '); 
-    std::string_view left = request.substr(0,pos);    
-    std::string_view rigth = request.substr(pos+1);    
-    if (left == "Bus"){
-        PrintBus(tansport_catalogue, left, rigth, output);    
-    } else if (left == "Stop"){
-        PrintStop(tansport_catalogue, left, rigth, output);
-    } 
-    output<<std::endl;
+void ParseAndPrintStat(const TransportCatalogue& catalogue, std::string_view request, std::ostream& output) {
+    size_t pos = request.find(' ');
+    std::string_view left = request.substr(0, pos);
+    std::string_view right = request.substr(pos + 1);
+    if (left == "Bus") {
+        PrintBus(catalogue, left, right, output);
+    } else if (left == "Stop") {
+        PrintStop(catalogue, left, right, output);
+    }
+    output << std::endl;
 }
 
-}
+} // namespace utility
