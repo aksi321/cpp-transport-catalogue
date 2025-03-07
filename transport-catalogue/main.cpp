57943df60bb4d8b042af23049ac12c2fe757efb6
aksi321 <96183002+aksi321@users.noreply.github.com>
2025-03-07 16:44:40 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include "json.h"
+#include "json_builder.h"
 #include "json_reader.h"
+#include "json.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
+#include <iostream>
 
 int main() {
     auto doc = json::Load(std::cin);
@@ -10,8 +11,8 @@ int main() {
     catalogue::TransportCatalogue catalogue;
     auto input_data = json_reader::ParseInputData(doc);
     json_reader::FillTransportCatalogue(input_data, catalogue);
-    
-    auto answer = json_reader::ProcessRequests(doc, catalogue);
+    renderer::RenderSettings render_settings = json_reader::ParseRenderSettings(doc);
+    auto answer = json_reader::ProcessRequests(doc, catalogue, render_settings);
     json::Print(answer, std::cout);
     
     return 0;
