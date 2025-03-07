5abb8a83fa576df6c7e622abccfc0e0ddd671211
aksi321 <96183002+aksi321@users.noreply.github.com>
2025-03-07 16:45:55 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -8,7 +8,6 @@
 
 namespace json_reader {
 
-// Структура для хранения данных об остановке, полученных из JSON.
 struct StopData {
     std::string name;
     double latitude;
@@ -16,26 +15,22 @@ struct StopData {
     std::map<std::string, int> road_distances;
 };
 
-// Структура для хранения данных о маршруте, полученных из JSON.
 struct BusData {
     std::string name;
     std::vector<std::string> stops;
     bool is_roundtrip = false;
 };
 
-// Структура, инкапсулирующая весь входной набор данных из JSON.
 struct InputData {
     std::vector<StopData> stops;
     std::vector<BusData> buses;
     renderer::RenderSettings render_settings;
 };
 
-// Преобразует входной JSON в структуру InputData
 InputData ParseInputData(const json::Document& doc);
-// Принимает уже подготовленный объект InputData и заполняет транспортный справочник.
 void FillTransportCatalogue(const InputData& input_data, catalogue::TransportCatalogue& catalogue);
 
-json::Document ProcessRequests(const json::Document& doc, const catalogue::TransportCatalogue& catalogue);
+json::Document ProcessRequests(const json::Document& doc, const catalogue::TransportCatalogue& catalogue, const renderer::RenderSettings& render_settings);
 
 renderer::RenderSettings ParseRenderSettings(const json::Document& doc);
 
