6d67f6d87fdda9a2ba9886a249245eb1b6fc739b
aksi321 <96183002+aksi321@users.noreply.github.com>
2025-04-08 17:09:58 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -6,27 +6,23 @@
 #include <set>
 #include <unordered_map>
 #include <map>
-#include "geo.cpp"
 #include <iostream>
 #include <functional>
 #include <utility>
+#include "geo.cpp"
 #include "domain.h"
 
 using domain::Stop;
 using domain::Bus;
 
 namespace catalogue {
-    
-    struct BusesForStop {
-        std::string request_status;
-        std::set<std::string_view> buses;
-    };
 
     struct BusCounted {
         size_t amount = 0;
         size_t unique = 0;
-        double length = 0;     
-        double geo_length = 0; 
+        double length = 0;
+        double geo_length = 0;
+
         bool IsEmpty() const {
             return (amount == 0 && unique == 0 && length == 0 && geo_length == 0);
         }
@@ -44,14 +40,15 @@ namespace catalogue {
     public:
         void AddStop(const std::string& name, double lat, double lng);
         void AddBus(const std::string& name, const std::vector<std::string_view>& stops, bool is_roundtrip);
-        void AddDistance(const std::string& stop_name, const std::string& neighbor, int distance);
+        void SetDistance(std::string_view stop_from, std::string_view stop_to, int distance);
         const std::unordered_map<std::string_view, Bus*>& GetAllBuses() const;
         const std::unordered_map<std::string_view, Stop*>& GetAllStops() const;
-        int GetDistance(const Stop* a, const Stop* b) const;
+        int GetDistance(const Stop* stop_from, const Stop* stop_to) const;
         BusCounted GetBusStatistics(std::string_view bus_name) const;
-        BusesForStop GetBusesForStop(std::string_view stop_name) const;
+        const std::set<std::string_view>& GetBusesForStop(std::string_view stop_name) const;
         const Stop* GetStop(std::string_view name) const;
         const Bus* GetBus(std::string_view name) const;
+
     private:
         BusCounted CountStation(std::string_view bus_name) const;
         void LinkBusToStops(std::string_view bus_name);
@@ -62,5 +59,7 @@ namespace catalogue {
         std::unordered_map<std::string_view, Bus*> bus_ptr_;
         std::unordered_map<std::string_view, std::set<std::string_view>> buses_for_stop_;
         std::unordered_map<std::pair<const Stop*, const Stop*>, int, StopPairHasher> distances_;
+        static const std::set<std::string_view>& EmptyBusSet(); 
     };
+
 } // namespace catalogue
